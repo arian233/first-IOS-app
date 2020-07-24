e0abc85bc5be8428fbfcb966ba6321f963e8edfa
Arian Ansari <esfahanarian@gmail.com>
2020-07-24 16:45:13 -0700

    first commit

---
+++ first basic app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  first basic app
+//
+//  Created by Arian on 2020-07-18.
+//  Copyright © 2020 Arian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
